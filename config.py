cdbfef030333ff2659b99f7e966e4fb5144b2ce8
v3896233055 <vgristiuk@gmail.com>
2026-02-26 10:49:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # config.py
 
 # --- CHIAVI API ---
-GEMINI_API_KEY = "AIzaSyA3RDa_KwFfhplR3lR0FNrz0eCrIQs2gMY"
-
+GEMINI_API_KEY = 
 # --- PERCORSI E GOOGLE SHEETS ---
 # Usa la 'r' davanti per evitare problemi con i simboli \ di Windows
-PATH_GOOGLE_CREDS = r"C:\Users\vgristiuk\Desktop\Progetto\creds.json"
-GOOGLE_SHEET_NAME = "Magazzino_Base"
-
+PATH_GOOGLE_CREDS = r
 # --- IMPOSTAZIONI SISTEMA ---
 LINGUA = "it-IT"
+
 SOGLIA_FUZZY = 85  # Precisione per correggere le parole (0-100)
